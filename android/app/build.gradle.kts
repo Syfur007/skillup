c5d737acc5a84e39dd8240a663553340423e97ab
Syfur007 <syfur.cse8.bu@gmail.com>
2025-10-19 01:59:31 +0600

    refactor: Update Android application ID and namespace

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.skillup"
+    namespace = "com.skillup.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.skillup"
+        applicationId = "com.skillup.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
