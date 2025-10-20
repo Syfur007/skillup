bba335e8e82d699268e0e50f1b1af69e14044248
Rovinota <tanvir.cse8.bu@gmail.com>
2025-10-21 02:54:55 +0600

    feat: implement authentication logic in AuthProvider and enhance LoginScreen UI with form validation

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../providers/auth_provider.dart';
+
+final _formKey = GlobalKey<FormState>();
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,16 +11,101 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _clearFields() {
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            Text("Login Screen", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-        
+            SizedBox(height: 50),
+            Flexible(
+              child: Text(
+                "Welcome to Skillup",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    child: TextFormField(
+                      controller: _emailController,
+                      validator: (value) {
+                        return AuthProvider().validateEmail(value);
+                      },
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.email),
+                        hintText: "Enter your email",
+                        labelText: "Email",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    child: TextFormField(
+                      controller: _passwordController,
+                      validator: (value) {
+                        return AuthProvider().validatePassword(value);
+                      },
+
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.password),
+                        labelText: "Password",
+                        hintText: "Enter your Password",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print('Email: ${_emailController.text}');
+                        print('Password: ${_passwordController.text}');
+                        _clearFields();
+                      }
+                    },
+                    child: Text("Login"),
+                  ),
+                  TextButton(onPressed: () {}, child: Text("Forgot Password?")),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text("Don't have an account? Sign up"),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
-      ));
+      ),
+    );
   }
 }
